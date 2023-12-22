5605ef12f30541b00e17244a5b3678f3bef87d35
Marco Salmo <137487624+Msalmo2@users.noreply.github.com>
2023-12-23 00:51:28 +0800

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,62 @@
+// Import the throttle function from lodash
+import throttle from 'lodash.throttle';
 
+// Throttle the function to be called at most once every 500 milliseconds
+const saveToLocalStorageThrottled = throttle(saveToLocalStorage, 500);
+
+
+document
+  .querySelector('.feedback-form')
+  .addEventListener('input', saveToLocalStorageThrottled);
+
+
+window.addEventListener('load', loadFromLocalStorage);
+
+
+document
+  .querySelector('.feedback-form')
+  .addEventListener('submit', function (event) {
+
+    event.preventDefault();
+
+
+    const formData = loadFromLocalStorage();
+
+
+    localStorage.removeItem('feedback-form-state');
+
+
+    document.querySelector('.feedback-form').reset();
+
+
+    console.log('Form Data:', formData);
+  });
+
+
+function saveToLocalStorage() {
+  const email = document.querySelector('[name="email"]').value;
+  const message = document.querySelector('[name="message"]').value;
+
+  const formData = { email, message };
+
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+
+function loadFromLocalStorage() {
+  const storedData = localStorage.getItem('feedback-form-state');
+
+  if (storedData) {
+    const formData = JSON.parse(storedData);
+
+
+    document.querySelector('[name="email"]').value = formData.email;
+    document.querySelector('[name="message"]').value = formData.message;
+
+    return formData;
+  }
+
+
+  return {};
+}
